345a2d3e02dffbfe7802e97f07b141ad7b2b7783
Alejo Ballen <aballenplays@gmail.com>
2022-06-25 18:40:41 -0400

    en este commti agregamos gran parte del Javascript, vamos agregando codigo cada ves mas XDXDXD

--- index.js
+++ index.js
@@ -6,10 +6,45 @@
 
     let todos = [];
     let lists = [
-        {id: '12121212', text: 'Colegio', count: 0},
-        {id: '12121212', text: 'Casa', count: 0},
-        {id: '12121212', text: 'Estudio Personal', count: 0}
+        {id: '200618', text: 'Colegio', count: 0},
+        {id: '200618', text: 'Casa', count: 0},
+        {id: '200618', text: 'Desarrollo Personal', count: 0}
     ];
 
+    document.addEventListener('DOMContentLoaded', e =>{
+        refreshUI();
+        lists.forEach( list => {
+            selectList.innerHTML += `<option value="${list.id}">${list.text}</option>`;
+        });
+    });
+
+    function Todo(id, text, list, completed){
+        return{id:id, text:text, list:list, completed:completed};
+    };
+
+    form.addEventListener('submit', e =>{
+        e.preventDefault();
+        const text = input.value.trim();
+        const listId =selectList.value;
+
+        if(text === '') return false;
+
+        const newTodo = new Todo(id= '200618', text, listId, false);
+        todos.push(newTodo);
+        input.value = '';
+
+        refreshUI();
+    });
     
+    function refreshUI(){
+        renderTodos();
+        renderLists();
+    };
+
+    function renderTodos(){
+        const todosContainer = document.querySelector('#todos');
+        todosContainer.innerHTML = '';
+    };
+
+    function renderLists(){};
 })();
