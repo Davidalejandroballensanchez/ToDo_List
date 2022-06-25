0524eab84c120d96c0b895d6d159b179f68a4eab
Alejo Ballen <aballenplays@gmail.com>
2022-06-25 16:30:54 -0400

    Este es nuestro segundo commit XDDDDDDD

---
+++ index.js
@@ -0,0 +1,15 @@
+(function(){
+    const input = document.querySelector('#itnew');
+    const form = document.querySelector('#formitnew');
+    const selectList = document.querySelector('#slist');
+    const listsContainer = document.querySelector('#lists');
+
+    let todos = [];
+    let lists = [
+        {id: '12121212', text: 'Colegio', count: 0},
+        {id: '12121212', text: 'Casa', count: 0},
+        {id: '12121212', text: 'Estudio Personal', count: 0}
+    ];
+
+    
+})();
