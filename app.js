52691d0d8c74eb2e88a8b6e01b8ffeae3a3e09de
Alejo Ballen <aballenplays@gmail.com>
2022-07-07 19:39:42 -0500

    Re iniciamos nuestro proyecto de Javascript llamado Lista Todo

---
+++ app.js
@@ -0,0 +1,28 @@
+const input = document.querySelector('input');
+const addBtn = document.querySelector('.btn-add');
+const ul = document.querySelector('ul');
+const empty = document.querySelector('.empty');
+
+addBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    
+    const text = input.value;
+
+    const li = document.createElement('li');
+    const p = document.createElement('p');
+    p.textContent = text;
+
+    li.appendChild(p);
+    ul.appendChild(li);
+});
+
+function addDeleteBtn () {
+    const deleteBtn = document.createElement('button');
+
+    deleteBtn.textContent = "X";
+    deleteBtn.className = "btn-delete";
+
+    deleteBtn.addEventListener('click', (e) => {
+        const item = e.target.parentElement;
+    }); 
+};
